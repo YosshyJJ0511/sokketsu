b7fc74ef806a91fc3ddfe473ade812e4bb7bc40f
YosshyJJ0511 <ec2-user@ip-172-31-44-58.ap-northeast-1.compute.internal>
2020-02-08 07:33:12 +0000

    日本語版を作成

--- app/controllers/sokketsu_controller.rb
+++ app/controllers/sokketsu_controller.rb
@@ -1,8 +1,9 @@
 class SokketsuController < ApplicationController
   #候補日の入力から開始
   def index
-    #@sokketsu = Sokketsu.all
   end
+  def update
+  end 
   def create
     #（保留）@sokketsu = Sokketsu.create(text: comment_params[:text], tweet_id: comment_params[:tweet_id], user_id: current_user.id)
     #（保留）redirect_to "/tweets/#{@comment.tweet.id}"   
